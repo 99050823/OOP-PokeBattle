ebb1ae24ea15588dd92d5585895b672cd68b55ba
99050823 <99050823@mydavinci.nl>
2022-04-21 12:35:16 +0200

    Initial Commit: Basic HTML and Styling

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+    <link rel="stylesheet" href="Style/style.css">
+</head>
+<body class="w3-black">
+    
+    <div id="container" class="w3-container">
+        <div class="top w3-row">
+            <div id="list" class="w3-col s6 w3-green w3-center">
+                <p>Pokemon List</p>
+            </div>
+
+            <div id="view" class="w3-col s5 w3-blue w3-center">
+                <p>Pokemon View</p>
+            </div>
+        </div>
+
+        <div class="bottom w3-row">
+            <div id="box" class="w3-col s8 w3-center">
+                <p>Battle Box</p>
+            </div>
+
+            <div id="active" class="w3-col s4 w3-gray w3-center">
+                <p>Active</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
